660f9ea42f3cdf4e00128549705407c42b56f642
jiyounglee-gloud <95066911+jiyounglee-gloud@users.noreply.github.com>
2022-03-11 11:55:32 +0900

    chore: jwt, swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		new SpringApplicationBuilder(DemoApplication.class)
+				.run();
 	}
 
+	@RequestMapping(value = "/robots.txt")
+	@ResponseBody
+	public String robots() {
+		return "User-agent: *\nDisallow: /\n";
+	}
 }
