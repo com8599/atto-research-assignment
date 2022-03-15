72f47eed303e9322e6a85db1baaaf1e231706115
jiyounglee-gloud <95066911+jiyounglee-gloud@users.noreply.github.com>
2022-03-15 12:50:59 +0900

    style: ReservationSaveRequestDto move

--- src/main/java/com/example/demo/dto/place/ReservationSaveRequestDto.java
+++ src/main/java/com/example/demo/dto/place/ReservationSaveRequestDto.java
@@ -1,18 +1,26 @@
-package com.example.demo.dto.reservation;
+package com.example.demo.dto.place;
 
 import com.example.demo.domain.place.Place;
 import com.example.demo.domain.place.Reservation;
 import com.example.demo.domain.user.Account;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+
+import java.util.Date;
 
 @Getter
 public class ReservationSaveRequestDto {
+    @Schema(description = "장소 id")
     private Long placeId;
+
+    @Schema(description = "예약시간")
+    private Date reserveAt;
 
     public Reservation toEntity(Place place, Account account) {
         return Reservation.builder()
                 .place(place)
                 .account(account)
+                .reserveAt(reserveAt)
                 .build();
     }
 }
