f1b12684bf4cf65bc71f4bcde0596b8549029e51
jiyounglee-gloud <95066911+jiyounglee-gloud@users.noreply.github.com>
2022-03-17 18:34:22 +0900

    refactor: edit view api add

--- src/main/java/com/example/demo/controller/view/AdminViewController.java
+++ src/main/java/com/example/demo/controller/view/AdminViewController.java
@@ -1,18 +1,39 @@
 package com.example.demo.controller.view;
 
+import com.example.demo.sevice.place.PlaceService;
+import com.example.demo.sevice.user.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
 @Controller
+@RequiredArgsConstructor
 public class AdminViewController {
+    private final AccountService accountService;
+    private final PlaceService placeService;
+
     @RequestMapping("user")
     public String user() {
         return "admin-user";
     }
 
+    @RequestMapping("user/edit/{id}")
+    public String userEdit(@PathVariable Long id, Model model) {
+        model.addAttribute("user", accountService.findById(id).getBody().getResult());
+        return "user-edit";
+    }
+
     @RequestMapping("place")
     public String place() {
         return "admin-place";
+    }
+
+    @RequestMapping("place/edit/{id}")
+    public String placeEdit(@PathVariable Long id, Model model) {
+        model.addAttribute("place", placeService.findById(id).getBody().getResult());
+        return "place-edit";
     }
 }
